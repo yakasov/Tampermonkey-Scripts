f2db623583c08b3d9ad733362533dfbe67649dde
James McDougall <james.mcdougall@theaccessgroup.com>
2022-10-21 09:26:36 +0100

    Update request functions

--- CRM Functions.user.js
+++ CRM Functions.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         CRM Functions
-// @version      1.1
+// @version      1.2
 // @description  Helpful functions for ProspectSoft CRM
 // @author       Yakasov
 // @updateURL    https://raw.githubusercontent.com/yakasov/Tampermonkey-Scripts/main/CRM%20Functions.user.js
@@ -9,14 +9,19 @@
 // @match        https://crm.prospect365-dev.com/
 // @match        https://crm.prospect365-qa.com/
 // @grant        GM_registerMenuCommand
-// @grant        GM_openInTab
 // ==/UserScript==
 
 const functions = {
+    "Clear local storage": clearLocalStorage,
     "Copy environment token": copyToken,
     "Toggle OData Lite": toggleODataLite,
     "Run GET request": runGetRequest,
+    "Run OData request": runODataRequest,
     "Change UI colour": changeColour,
+};
+
+function clearLocalStorage() {
+    localStorage.clear();
 };
 
 function copyToken() {
@@ -28,15 +33,27 @@ function toggleODataLite() {
 };
 
 function runGetRequest() {
-    let url = prompt("Enter URL to send GET request to:", "https://api-v1.prospect365-dev.com/Users");
+    const url = prompt("Enter URL to send GET request to:", "https://api-v1.prospect365-dev.com/Users");
     $.ajax({
+        method: "GET",
         url: url,
+        synchronous: true,
         headers: {"authorization":"Bearer " + environment.token, "content-type":"application/json; charset=utf-8","x-core-query":"true","x-locale":"en-gb"},
         dataType: "json",
-        success: function(response) {
-            GM_openInTab("data:text/json," + encodeURIComponent(JSON.stringify(response, null, "\t")), "_blank");
-        }
+        success: function(response) { copyJsonUrlToClipboard(response) },
     });
+};
+
+async function runODataRequest() {
+    const entity = prompt("Enter entity to query in OData:\nProspectSoft.OData.", "SalesLedger");
+    const response = await new Api.Query(ProspectSoft.OData[entity]);
+    await copyJsonUrlToClipboard(response);
+};
+
+async function copyJsonUrlToClipboard(response) {
+    const jsonText = encodeURIComponent(JSON.stringify(response, null, "\t"));
+    await navigator.clipboard.writeText("data:text/json," + jsonText);
+    alert("URL copied to clipboard!");
 };
 
 function changeColour() {
@@ -46,3 +63,4 @@ function changeColour() {
 for (const [key, val] of Object.entries(functions)) {
     GM_registerMenuCommand(key, val);
 };
+
